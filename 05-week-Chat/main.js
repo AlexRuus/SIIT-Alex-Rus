bff5fc6e160715cbacf3a2649107f432ba974f68
AlexRuus <alexrus2503@gmail.com>
2021-10-16 21:38:13 +0300

    Second half of the homework

--- 05-week-Chat/main.js
+++ 05-week-Chat/main.js
@@ -1,8 +1,16 @@
 const sendBtn = document.querySelector('#send-btn');
 const userText = document.querySelector('#user-typing-area');
-
+const msgContainer = document.querySelector('#message-container');
 
 sendBtn.addEventListener('click',function(){
     let text = userText.value;
     console.log(text);
-})
+    createUserBox();
+});
+
+
+function createUserBox(){
+    let message = document.createElement('div');
+    message.classList.add('user-box');
+    msgContainer.appendChild(message);
+}
