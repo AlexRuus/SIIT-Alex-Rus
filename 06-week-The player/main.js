253d7051bdc36815c5cd0799000bcb62b17a3783
AlexRuus <alexrus2503@gmail.com>
2021-10-21 01:21:34 +0300

    Small update

--- 06-week-The player/main.js
+++ 06-week-The player/main.js
@@ -13,36 +13,40 @@ window.addEventListener('keydown', (e) => {
             player.style.left = parseInt(player.style.left) - moveBy + 'px';
             console.log(player.style.left + ' left');
             if(player.style.left == -10 + 'px'){
-                alert('you lost')
-                player.style.left = 0 + 'px'
-                player.style.top = 0 + 'px'
+                player.style.left = parseInt(player.style.left) + moveBy + 'px';
+                // alert('you lost')
+                // player.style.left = 0 + 'px'
+                // player.style.top = 0 + 'px'
             }
             break;
         case 'ArrowRight':
             player.style.left = parseInt(player.style.left) + moveBy + 'px';
             console.log(player.style.left + ' right');
             if(player.style.left == 400 + 'px'){
-                alert('you lost')
-                player.style.left = 0 + 'px'
-                player.style.top = 0 + 'px'
+                player.style.left = parseInt(player.style.left) - moveBy + 'px';
+                // alert('you lost')
+                // player.style.left = 0 + 'px'
+                // player.style.top = 0 + 'px'
             }
             break;
         case 'ArrowUp':
             player.style.top = parseInt(player.style.top) - moveBy + 'px';
             console.log(player.style.top + ' up');
             if(player.style.top == -10 + 'px'){
-                alert('you lost')
-                player.style.top = 0 + 'px'
-                player.style.left = 0 + 'px'
+                player.style.top = parseInt(player.style.top) + moveBy + 'px';
+                // alert('you lost')
+                // player.style.top = 0 + 'px'
+                // player.style.left = 0 + 'px'
             }
             break;
         case 'ArrowDown':
             player.style.top = parseInt(player.style.top) + moveBy + 'px';
             console.log(player.style.top + ' down');
             if(player.style.top == 400 + 'px'){
-                alert('you lost')
-                player.style.top = 0 + 'px'
-                player.style.left = 0 + 'px'
+                player.style.top = parseInt(player.style.top) - moveBy + 'px';
+                // alert('you lost')
+                // player.style.top = 0 + 'px'
+                // player.style.left = 0 + 'px'
             }
             break;
     }
